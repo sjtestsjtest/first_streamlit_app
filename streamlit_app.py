9c0b0d0cc859f30bf15b022e2a467707c44735ba
sjtestsjtest <142532673+sjtestsjtest@users.noreply.github.com>
2023-08-17 14:14:26 -0600

    Update streamlit_app.py
    fixing things up to look like example

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,13 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 
 # put a pick list here....
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# display the table
 streamlit.dataframe(my_fruit_list)
+
+
